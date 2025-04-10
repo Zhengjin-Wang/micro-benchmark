b348d2365b5d8d4f26c85a955c70c6e4d157f6cb
Zhengjin-Wang <1054129023@qq.com>
2025-04-10 08:19:03 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,74 @@
+#include <chrono>
+#include <vector>
+#include <iostream>
+#include <memory>
+
+#include "src/operator/join_hash.hpp"
+#include "src/storage/storage.hpp"
+#include "src/storage/segment.hpp"
+
+#define OUTPUT_DATA 0
+
+int main() {
+
+    size_t m = 309526; // r_table_size
+    size_t n = 12001215; // s_table_size
+    size_t pk_lower_bound = 1;
+    size_t pk_upper_bound = 400000;
+    size_t CHUNK_SIZE = 65536;
+
+
+    // 定义表结构：三列（int, float, string）
+    std::vector<ColumnDefinition> r_column_defs = {
+        {int{}, true, 0, 0},
+        {int{}, false, 1, 100},
+        {std::string{}, false, 0, 0}
+    };
+    const auto& r_table = std::make_shared<Table>(r_column_defs, CHUNK_SIZE);
+
+    std::vector<ColumnDefinition> s_column_defs = {
+        {int{}, false, (float) pk_lower_bound, (float) pk_upper_bound},
+        {int{}, false, 1, 50},
+        {float{}, false, 900.0, 9000.0},
+        {float{}, false, 0.01, 0.10},
+        {float{}, false, 0.01, 0.10},
+        {int{}, false, 1, 7}
+    };
+    const auto& s_table = std::make_shared<Table>(s_column_defs, CHUNK_SIZE);
+
+    auto start = std::chrono::high_resolution_clock::now();
+    r_table->generate_data(m); // 生成m行数据
+    s_table->generate_data(n); // 生成n行数据
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration<double>(end - start).count();
+    std::cerr << "Generate data time: " << duration << "s" << std::endl;
+    if (OUTPUT_DATA){
+        r_table->output_data("r_table.csv", {0});
+        s_table->output_data("s_table.csv", {0});
+    }
+    std::pair<ColumnID, ColumnID> column_ids({0, 0});
+
+    start = std::chrono::high_resolution_clock::now();
+    join_hash<int, int, int>(r_table, s_table, column_ids, 10);
+    end = std::chrono::high_resolution_clock::now();
+    duration = std::chrono::duration<double>(end - start).count();
+    std::cerr << "hash_join time: " << duration << "s" << std::endl;
+    // 打印数据
+    // const auto& r_chunk = r_table.chunks()[0];
+    // const auto& r_segment =    std::dynamic_pointer_cast<IntSegment>(r_chunk->get_segment(0));
+    //
+    // const auto& s_chunk = s_table.chunks()[0];
+    // const auto& s_segment =    std::dynamic_pointer_cast<IntSegment>(s_chunk->get_segment(0));
+
+    // for (size_t i = 0; i < m; ++i) {
+    //     int id = r_segment->at(i);
+    //     std::cout << "r-id: " << id << std::endl;
+    // }
+    //
+    // for (size_t i = 0; i < n; ++i) {
+    //     int id = s_segment->at(i);
+    //     std::cout << "s-id: " << id << std::endl;
+    // }
+
+    return 0;
+}
---
+++ src/storage/segment.hpp
@@ -0,0 +1,185 @@
+//
+// Created by lilac on 25-4-10.
+//
+
+#ifndef SEGMENT_HPP
+#define SEGMENT_HPP
+#include <variant>
+#include <string>
+#include <vector>
+#include <random>
+#include <memory>
+
+
+// 支持的基础数据类型
+using DataType = std::variant<int, float, std::string>;
+using ChunkOffset = int32_t;
+
+// 列定义（名称 + 类型）
+struct ColumnDefinition {
+    // std::string name;
+    DataType type;
+    bool is_pk;
+    float range_lower_bound; // only valid for int and float, invalid for pk and string
+    float range_upper_bound; // only valid for int and float, invalid for pk and string
+};
+
+
+class SegmentPosition final {
+public:
+    SegmentPosition()
+    : _value(DataType()), _null_value(false), _chunk_offset() {}
+
+    SegmentPosition(const DataType& value, const bool null_value, const ChunkOffset& chunk_offset)
+        : _value{value}, _null_value{null_value}, _chunk_offset{chunk_offset} {}
+
+    const DataType& value() const {
+        return _value;
+    }
+
+    bool is_null() const {
+        return _null_value;
+    }
+
+    ChunkOffset chunk_offset() const {
+        return _chunk_offset;
+    }
+
+private:
+    // The alignment improves the suitability of the iterator for (auto-)vectorization
+    alignas(8) DataType _value;
+    alignas(8) bool _null_value;
+    alignas(8) ChunkOffset _chunk_offset;
+};
+
+// 段（Segment）基类 - 代表单列数据存储
+class BaseSegment {
+public:
+    explicit BaseSegment(bool is_pk, float range_lower_bound, float range_upper_bound):
+    _is_pk(is_pk), _range_lower_bound(range_lower_bound), _range_upper_bound(range_upper_bound){};
+    virtual ~BaseSegment() = default;
+    virtual void generate_random(size_t num_rows) = 0; // 随机生成数据
+    bool is_pk() const { return _is_pk; }
+
+    static std::shared_ptr<BaseSegment> create_segment(const DataType& type, bool is_pk, float range_lower_bound, float range_upper_bound);
+
+    const SegmentPosition at(int32_t i) const {
+        return {_data[i], _null_values[i], i};
+    }
+
+    const std::vector<DataType>& data() const {
+        return _data;
+    }
+
+protected:
+    bool _is_pk;
+    float _range_lower_bound;
+    float _range_upper_bound;
+    std::vector<DataType> _data;
+    std::vector<bool> _null_values;
+};
+
+// 具体段类型
+// 整数段
+class IntSegment : public BaseSegment {
+public:
+    using BaseSegment::BaseSegment;
+
+    void generate_pk(size_t num_rows, size_t start_offset = 1) {
+        for (int i = (int) start_offset; i <= (int) (start_offset + num_rows - 1); ++i) {
+            _data.emplace_back(i);
+        }
+        _null_values.resize(num_rows);
+    }
+
+    void generate_random(size_t num_rows) override {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<> dis(static_cast<int>(_range_lower_bound), static_cast<int>(_range_upper_bound));
+        // std::cout << static_cast<int>(_range_lower_bound) << " " << static_cast<int>(_range_upper_bound) << std::endl;
+        _data.clear();
+        for (size_t i = 0; i < num_rows; ++i) {
+            int val = dis(gen);
+            // if (val < 1) std::cout << "less than 1" << std::endl;
+            _data.emplace_back(val);
+        }
+        _null_values.resize(num_rows);
+    }
+
+    int at(size_t i) const {
+        return std::get<int>(data()[i]);
+    }
+
+};
+
+// 浮点数段
+class FloatSegment : public BaseSegment {
+public:
+    using BaseSegment::BaseSegment;
+
+    void generate_random(size_t num_rows) override {
+        std::random_device rd;
+        std::mt19937 gen(rd());    // 伪随机数生成器
+        std::uniform_real_distribution<float> dis(_range_lower_bound, _range_upper_bound);
+
+        _data.clear();
+        for (size_t i = 0; i < num_rows; ++i) {
+            _data.emplace_back(dis(gen)); // 存储为 float 类型
+        }
+        _null_values.resize(num_rows);
+    }
+
+    float at(size_t i) const {
+        return std::get<float>(data()[i]);
+    }
+
+
+};
+
+
+// 字符串段
+class StringSegment : public BaseSegment {
+public:
+    using BaseSegment::BaseSegment;
+
+    void generate_random(size_t num_rows) override {
+        const std::string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        std::uniform_int_distribution<> len_dis(5, 15);    // 随机生成字符串长度
+        std::uniform_int_distribution<> char_dis(0, chars.size() - 1);
+
+        _data.clear();
+        for (size_t i = 0; i < num_rows; ++i) {
+            std::string str;
+            int len = len_dis(_gen);
+            for (int j = 0; j < len; ++j) {
+                str += chars[char_dis(_gen)];
+            }
+            _data.emplace_back(str); // 存储为 string 类型
+        }
+        _null_values.resize(num_rows);
+    }
+
+    std::string at(size_t i) const {
+        return std::get<std::string>(data()[i]);
+    }
+
+private:
+    std::mt19937 _gen{std::random_device{}()};
+};
+
+
+// Chunk：生成segment
+std::shared_ptr<BaseSegment> BaseSegment::create_segment(const DataType& type, bool is_pk, float range_lower_bound, float range_upper_bound) {
+    if (std::holds_alternative<int>(type)) {
+        return std::make_shared<IntSegment>(is_pk, range_lower_bound, range_upper_bound);
+    }
+    else if (std::holds_alternative<float>(type)) {
+        return std::make_shared<FloatSegment>(is_pk, range_lower_bound, range_upper_bound);
+    }
+    else if (std::holds_alternative<std::string>(type)) {
+        return std::make_shared<StringSegment>(is_pk, range_lower_bound, range_upper_bound);
+    }
+
+    throw std::runtime_error("Unsupported data type");
+}
+#endif //SEGMENT_HPP
